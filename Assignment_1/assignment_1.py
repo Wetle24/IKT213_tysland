171c1472c75dfd15b6b1d377892c7bea52918f2a
Vetle Andreas Tysland <vetleat@uia.no>
2025-10-08 22:04:22 +0200

    Added pictures

--- Assignment_1/assignment_1.py
+++ Assignment_1/assignment_1.py
@@ -1,9 +1,10 @@
 import numpy as np
 import cv2
+import os
 
 def print_image_information(image):
     #function code here
-    img = cv2.imread("lena-1.png",1)
+    img = cv2.imread(image)
 
     height, width, channel = img.shape
     print('height ', height)
@@ -14,7 +15,7 @@ def print_image_information(image):
 def main():
     #all code here
     print("Hello World")
-    print_image_information("lena-1.png")
+    print_image_information("assignment_1\lena-1.png")
     return 0
 
 main()
