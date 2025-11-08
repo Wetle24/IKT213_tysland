8c5887fdda396f9c504298467b7901dda64f8d99
Vetle Andreas Tysland <vetleat@uia.no>
2025-11-09 00:57:29 +0100

    Finished assignment 1 code

--- Assignment_1/assignment_1.py
+++ Assignment_1/assignment_1.py
@@ -7,14 +7,17 @@ def print_image_information(image):
     img = cv2.imread(image)
 
     height, width, channel = img.shape
-    print('height ', height)
-    print('width ', width)
-    print('channel ', channel)
+    size = img.size
+    data_type = img.dtype
+    print('A: height ', height)
+    print('B: width ', width)
+    print('C: channel ', channel)
+    print('D. size ', size)
+    print('E. data type ', data_type)
     return
 
 def main():
     #all code here
-    print("Hello World")
     print_image_information("assignment_1\lena-1.png")
     return 0
 
