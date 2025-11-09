957eed6858ad3316dfde623b769c3263c6dcca91
Vetle Andreas Tysland <vetleat@uia.no>
2025-11-09 03:29:00 +0100

    Finished V

--- Assignment_1/assignment_1_4.py
+++ Assignment_1/assignment_1_4.py
@@ -3,7 +3,6 @@ import cv2
 import os
 
 def print_image_information(image):
-    #function code here
     img = cv2.imread(image)
 
     height, width, channel = img.shape
@@ -17,7 +16,6 @@ def print_image_information(image):
     return
 
 def main():
-    #all code here
     print_image_information("assignment_1\lena-1.png")
     return 0
 
---
+++ Assignment_1/assignment_1_5.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+
+cam = cv2.VideoCapture(0)
+
+fps = int(cam.get(cv2.CAP_PROP_FPS))
+frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+#got some errors with the path, so I had to do this instead of just writing in the path in the "open"
+output_path = os.path.join("solutions", "camera_outputs.txt")
+
+with open(output_path, "w") as output_file:
+    output_file.write(f"A: fps: {fps}\n")
+    output_file.write(f"B: height: {frame_height}\n")
+    output_file.write(f"C: width: {frame_width}\n") 
+
+while True:
+    ret, frame = cam.read()
+
+    cv2.imshow('Camera', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
